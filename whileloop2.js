dcb7d875a2ed83b573b730bb3d03b72f2b25bdd2
gabocode2907 <83044398+gabocode2907@users.noreply.github.com>
2021-04-25 21:15:22 -0500

    Update

---
+++ whileloop2.js
@@ -0,0 +1,9 @@
+var b = 2;
+while (b < 30) {       
+    if (b > 10) {        
+        b = b + 6;    
+    }    
+    b += 3;
+}
+console.log(b);
+//console result: 38
---
+++ whileloop3.js
@@ -0,0 +1,11 @@
+var c = 50;
+while (c > 0) {       
+    if (c > 47) {        
+        c--;    
+    }    
+    else {         
+        c -= 20;    
+    }
+}
+console.log(c);
+//console result: -13
